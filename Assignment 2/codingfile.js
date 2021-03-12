7e472c1ffa0863019dfbe15329d7783589a60b56
rahul-glitch <68232141+rahul-glitch@users.noreply.github.com>
2021-03-12 15:12:37 +0530

    Update codingfile.js

--- Assignment 2/codingfile.js
+++ Assignment 2/codingfile.js
@@ -1,6 +1,11 @@
-const car = {
-  brand: 'Ford',
-  model: 'Fiesta'
-}
 
-module.exports = car
+module.exports = {
+  dbconnection: function()
+  {
+  const mongoose=require("mongoose"); 
+  mongoose.connect("mongodb://localhost:27017/my_database",{ useNewUrlParser: true , useUnifiedTopology: true })
+  .then( () => console.log("CONNECTION DONE"))
+  .catch((err) => console.log(err));
+  }
+  }
+  
